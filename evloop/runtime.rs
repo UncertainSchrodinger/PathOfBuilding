0791e00cd23d45fb7b4bb5b2744e620c6f5a5d7c
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-20 18:44:07 +0200

    Skeleton support for lua
    Shitty part is that we need to have luajit in our system and cannot just
    import/vendor the code in.
    
    Demonstrates running code etc. Next up bindings to PoB.

--- evloop/runtime.rs
+++ evloop/runtime.rs
@@ -1,7 +1,22 @@
+use rlua::{Lua, Result};
+
 pub struct PathOfBuilding {}
 
 impl PathOfBuilding {
-    pub fn start(self) {
-        println!("Event loop starting");
+    pub fn start(self) -> Result<()> {
+        let lua = Lua::new();
+        lua.context(|lua_ctx| {
+            // add `some_directory` to the package path
+            lua_ctx
+                .load("package.path = package.path .. ';./lua/?.lua'")
+                .exec()?;
+
+            // require a module located in the newly added directory
+            lua_ctx.load("require'main'").exec()?;
+
+            Ok(())
+        })?;
+
+        Ok(())
     }
 }
