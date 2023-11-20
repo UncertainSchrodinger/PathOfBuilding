0231c49a1d6d801f59ab0d3c1269de1185a61ff6
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-20 18:07:03 +0200

    Add rust project skeleton

---
+++ evloop/main.rs
@@ -0,0 +1,8 @@
+use crate::runtime::PathOfBuilding;
+
+mod runtime;
+
+fn main() {
+    let pob = PathOfBuilding {};
+    pob.start();
+}
---
+++ evloop/runtime.rs
@@ -0,0 +1,7 @@
+pub struct PathOfBuilding {}
+
+impl PathOfBuilding {
+    pub fn start(self) {
+        println!("Event loop starting");
+    }
+}
