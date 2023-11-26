f94277d607ee4534bf563be1e375dd748da5d895
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-26 22:37:13 +0200

    rt: Add event loop through winit
    This initializes the window for rendering and gives us an event loop to
    use for other processing. Libuv for example could be plugged here.
    
    Loop runs only once a second just to make developing easier. I rely on
    the debug logs for now and they're not really readable at 180Hz.

--- evloop/runtime.rs
+++ evloop/runtime.rs
@@ -1,4 +1,10 @@
-use std::{fs, path::Path, result, sync::Arc};
+use std::{fs, path::Path, result, sync::Arc, thread, time};
+
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 use chrono::prelude::*;
 use curl::easy::{Easy2, Handler, WriteError};
@@ -468,9 +474,56 @@ impl PathOfBuilding {
         println!("Succesfully initialized Path Of Building");
 
         let launch = globals.get::<_, Table>("launch").unwrap();
-        println!("Generating frame");
         let on_frame: Function = launch.get::<_, Function>("OnFrame")?;
         on_frame.call::<_, ()>(launch)?;
+
+        // The event loop
+        let event_loop = EventLoop::new().unwrap();
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+        // Run the loop even if we have no events.
+        event_loop.set_control_flow(ControlFlow::Wait);
+
+        // TODO: This never returns and we cannot propagate back to main. We need to catch all
+        // unrecoverable errors inside the event loop, notify user of the error and then exit the
+        // loop.
+        event_loop.run(move |event, elwt| {
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    println!("The close button was pressed; stopping");
+                    elwt.exit();
+                }
+                Event::AboutToWait => {
+                    // Application update code.
+
+                    // Queue a RedrawRequested event.
+                    //
+                    // You only need to call this if you've determined that you need to redraw, in
+                    // applications which do not always need to. Applications that redraw continuously
+                    // can just render here instead.
+                    window.request_redraw();
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    // TODO: Just for testing. Need to move this behind a better api and cache
+                    // these lookups.
+                    println!("Generating frame");
+                    let launch = globals.get::<_, Table>("launch").unwrap();
+                    let on_frame: Function = launch.get::<_, Function>("OnFrame").unwrap();
+                    on_frame.call::<_, ()>(launch).unwrap();
+
+                    // FIXME: Hack to slowdown the loop
+                    let millis = time::Duration::from_millis(1000);
+                    thread::sleep(millis);
+                }
+                _ => (),
+            }
+        });
 
         Ok(())
     }
